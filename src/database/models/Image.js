54d0fabe43a392504ee9fe4e2e5c1753b797d8e0
Cristian Zamora <cristianelnum10@gmail.com>
2023-10-30 16:45:46 -0300

    modelo de la tabla Images, con sus respectivas relaciones

---
+++ src/database/models/Image.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, dataTypes) => {
+    const Image = sequelize.define("Image", {
+        id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING(300),
+            allowNull: false
+        },
+        color_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull: false
+        },
+        product_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull: false
+        },
+
+    }, {
+        timestamps: false, 
+        tableName: "images",
+    })
+
+    Image.associate = models => {
+        Image.belongsTo(models.Product, {
+            as: "producto",
+            foreignKey: "product_id"
+        })
+
+        Image.belongsTo(models.Color, {
+            as: "colorProducto",
+            foreignKey: "color_id"
+        })
+    }
+
+    return Image
+}
