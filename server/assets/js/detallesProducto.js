b02c39818de887a9922b9ee95a640e94b30bd8a8
Cristian Zamora <cristianelnum10@gmail.com>
2023-12-16 12:49:24 -0300

    carrusel del listado de productos relacionados de la vista [detallesProductos.ejs]

--- server/assets/js/detallesProducto.js
+++ server/assets/js/detallesProducto.js
@@ -9,3 +9,41 @@ imagesAside.addEventListener("mouseover", (e) => {
         mainImage.src = e.target.src
     }
 })
+
+//       ----------
+// --- -- Carrusel -- ---
+const carruselWrapper = document.querySelector(".carousel-wrapper")
+const leftButton = document.querySelector(".control-left")
+const rightButton = document.querySelector(".control-right")
+
+const quantityProducts = carruselWrapper.getAttribute("data-product-length")
+const distance = 100 / (quantityProducts / 4)
+let counter = 1
+
+rightButton.addEventListener("click", () => {
+    const translate = distance * (counter)
+    carruselWrapper.style.transform = `translateX(-${translate}%)`
+    counter++
+
+    if (counter === Math.ceil(quantityProducts / 4)) {
+        rightButton.classList.add("hidden")
+    }
+
+    if (counter !== 1) {
+        leftButton.classList.remove("hidden")
+    }
+})
+
+leftButton.addEventListener("click", () => {
+    const translate = distance * (counter - 1) * (-1) + distance
+    carruselWrapper.style.transform = `translateX(${translate}%)`
+    counter--
+
+    if (counter === 1) {
+        leftButton.classList.add("hidden")
+    }
+
+    if (counter < Math.ceil(quantityProducts / 4)) {
+        rightButton.classList.remove("hidden")
+    }
+})
