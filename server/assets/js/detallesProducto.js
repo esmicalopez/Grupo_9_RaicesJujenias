59db1ef1fa5560075aecf2ab3ab0a003be43aed7
Cristian Zamora <cristianelnum10@gmail.com>
2023-12-16 22:24:20 -0300

    detalles del producto [terminado]

--- server/assets/js/detallesProducto.js
+++ server/assets/js/detallesProducto.js
@@ -1,14 +1,14 @@
-// const imagesAside = document.querySelector(".img-aside")
-// const mainImage = document.querySelector(".container-img > img")
+const imagesAside = document.querySelector(".img-aside")
+const mainImage = document.querySelector(".container-img > img")
 
-// imagesAside.addEventListener("mouseover", (e) => {
-//     if (!e.target.classList.contains("selected-image") && e.target.tagName === "IMG") {
-//         document.querySelector(".selected-image").classList.remove("selected-image")
-//         e.target.classList.add("selected-image")
+imagesAside.addEventListener("mouseover", (e) => {
+    if (!e.target.classList.contains("selected-image") && e.target.tagName === "IMG") {
+        document.querySelector(".selected-image").classList.remove("selected-image")
+        e.target.classList.add("selected-image")
 
-//         mainImage.src = e.target.src
-//     }
-// })
+        mainImage.src = e.target.src
+    }
+})
 
 // //       ----------
 // // --- -- Carrusel -- ---
